c17c9188a062d9b63fed7056ee437edb5f6b9024
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-25 15:49:17 +0100

    inserita struttura carousel

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 /* creazione slider immagini */
+var app = new Vue ({
+  el: '#app',
+  data: {
+    images: ['https://siviaggia.it/wp-content/uploads/sites/2/2020/07/scala-dei-turchi.jpg',
+    'https://sicilyintour.com/wp-content/uploads/2020/05/65911.jpg',
+    'https://www.archeome.it/wp-content/uploads/2020/06/valle-dei-templi.jpg'
+  ],
+  imgIndex: 0,
+  }
+}
+
+);
