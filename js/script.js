d004c5bfef7cec4b464f9ee4c96c7c05a52c56ca
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-25 16:16:00 +0100

    inserito bullet

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ var app = new Vue ({
   data: {
     images: ['https://siviaggia.it/wp-content/uploads/sites/2/2020/07/scala-dei-turchi.jpg',
     'https://sicilyintour.com/wp-content/uploads/2020/05/65911.jpg',
-    'https://www.archeome.it/wp-content/uploads/2020/06/valle-dei-templi.jpg'
+    'https://www.archeome.it/wp-content/uploads/2020/06/valle-dei-templi.jpg',
   ],
   imgIndex: 0
  },
   methods: {
-    nextImage: function (){
+    nextImage: function(){
       this.imgIndex++;
       if (this.imgIndex == this.images.length){
         this.imgIndex = 0;
       }
     },
-    prevImage: function (){
+    prevImage: function(){
       this.imgIndex--;
       if (this.imgIndex < 0){
         this.imgIndex = this.images.length - 1;
